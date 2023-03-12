33af49eb2e56f24564ea593defd60c1269830f0c
NEPerova <natali276@yandex.ru>
2023-03-12 16:45:38 +0300

    credit22

--- src/main/java/credit.java
+++ src/main/java/credit.java
@@ -5,6 +5,8 @@ public class credit {
 
         public static void main (String[] args) {
             double summCredit;
+            double stavka;
+            double payment;
             try {
                 System.out.println("Введите сумму кредита");
                 Scanner scaner1 = new Scanner(System.in);
@@ -12,36 +14,40 @@ public class credit {
 
                 System.out.println("Введите процентную ставку");
                 Scanner scaner2 = new Scanner(System.in);
-                double stavka = scaner2.nextDouble();
+                stavka = scaner2.nextDouble();
 
                 System.out.println("Введите сумму ежемесячного платежа");
                 Scanner scaner3 = new Scanner(System.in);
-                double payment = scaner3.nextDouble();
+                payment = scaner3.nextDouble();
 
                 double ostatokOD;
                 double percent;
+                int percentSumm;
 
                 ostatokOD = summCredit;
                 double percentSum = 0;
                 double colPy = 0;
                 for (int i = 1; ostatokOD > 0; i++) {
-                    percent = ostatokOD / 100 * stavka / 365 * 30; //процент за месяц
+                    percent = ostatokOD / 100 * stavka / 365 * 30.42; //% за месяц
                     double paymentOD = payment - percent; // сумма от ежем.платежа которая пойдет в погашение основного долга
 
                     if (ostatokOD >= paymentOD) {
-                        ostatokOD -= paymentOD; // остаток основного долга
+                        ostatokOD -= paymentOD; // остаток ОД
                     } else if (ostatokOD < paymentOD) {
-                        ostatokOD = 0; // остаток основного долга
+                        ostatokOD = 0; // остаток ОД
                     }
 
                     percentSum += percent;
+
                     colPy = i;
 
-                    System.out.println(colPy);
-                    System.out.println(percentSum);
-                    System.out.println(ostatokOD);
+
 
                 }
+                percentSumm = (int)Math.ceil(percentSum);
+                System.out.println("Для погашения потребуется " + colPy + " мес.");
+                System.out.println("Переплата по процентам составит " + percentSumm + " руб.");
+//                System.out.println(ostatokOD);
             } catch (InputMismatchException exception) {
                 System.out.println("Введите коректные данные!");
                 ;
